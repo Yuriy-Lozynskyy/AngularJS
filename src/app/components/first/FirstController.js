b8a364fd1e303f71317e0e1342330b317786371a
Yuriy Lozynskyy <Yuriy Lozynskyy>
2016-03-22 20:26:56 +0200

    modified firstController function and index.html

--- src/app/components/first/FirstController.js
+++ src/app/components/first/FirstController.js
@@ -1,10 +1,10 @@
 angular.module("simpleChatApplication").controller("FirstController",FirstController);
 function FirstController($scope){
-  function test(){
-    for(var i=0;i<10;i++){
-      console.log(i);
-    }
-  }
-  test();
-  $scope.messages=['first message', 'second message', 'third message', 'fourth message'];
-}
+     $scope.messages=[];
+     $scope.text='';
+
+        $scope.input=function(){
+
+          $scope.messages.push($scope.text);
+     };
+   };
