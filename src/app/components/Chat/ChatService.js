484281aad867ee65123ad93fc0532ec4d5774e95
Yuriy Lozynskyy <Yuriy Lozynskyy>
2016-03-29 17:40:53 +0300

    Injected $http service in chatService

--- src/app/components/Chat/ChatService.js
+++ src/app/components/Chat/ChatService.js
@@ -11,15 +11,13 @@ function chatService($http){
         method : "GET",
         url : "http://simple-chat-example.herokuapp.com/v1/messages"
     }).then(function mySucces(response) {
-        messages = response.data;
+          for (var i=0;i<=response.data.messages.length;i++){
+            messages.push(response.data.messages[i]);
+      };
     }, function myError(response) {
-        messages = response.statusText;
+        messages = response.statusText.messages;
     });
   };
-
-    /*function getMessages(){
-		return messages;
-	};*/
 
   function sendMessage(message){
     messages.push(message);
